ea1413275af56e86da6b8ddc825bd26a21c33756
Fabio <fabioescobarardila@hotmail.com>
2024-02-10 12:49:32 -0500

    Complete: i18nSelected Pipe

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'products-basics-page',
+  selector: 'pages-basics-page',
   templateUrl: './basics-page.component.html',
 })
 export class BasicsPageComponent {
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-uncommon-page',
+  selector: 'pages-uncommon-page',
   templateUrl: './uncommon-page.component.html'
 })
 export class UncommonPageComponent {
+public name: string = 'Fabio'
+public gender: 'male' | 'female' = 'male'
+public genderMap = {
+  male : 'invitarlo',
+  female: 'invitarla'
+}
+
+changeClient(){
+  this.name = "Yulieth"
+  this.gender = "female"
+
+}
 
 }
