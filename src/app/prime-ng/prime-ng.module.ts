e8330a4986f91db6b09169bac6cf14d9ddf4d65d
Fabio <fabioescobarardila@hotmail.com>
2024-02-10 01:07:42 -0500

    Pipes: uppercase, lowercase, titlecase

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './basics-page.component.html',
 })
 export class BasicsPageComponent {
-
+  public name: string = 'Alejandro Escobar';
+  public date: Date = new Date();
+short: string|undefined;
 
 }
