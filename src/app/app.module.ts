1c83e5abbb6ec4135ffac34eff116709db4c54eb
Fabio <fabioescobarardila@hotmail.com>
2024-02-10 12:22:45 -0500

    Pipes: number, currency, percent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,13 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import localEsCO  from "@angular/common/locales/es-CO";
+import localFrCa  from "@angular/common/locales/fr-CA";
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localEsCO)
+registerLocaleData(localFrCa)
 
 @NgModule({
   declarations: [
@@ -18,7 +25,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-CO'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './numbers-page.component.html'
 })
 export class NumbersPageComponent {
+  public totalSells: number = 2567789.5567
+  public sellsPercentage: number = 0.4855
 
 }
