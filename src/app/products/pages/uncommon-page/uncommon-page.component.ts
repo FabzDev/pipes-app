da4e05e43cc9798d844692f7ce135b1db16cbcb0
Fabio <fabioescobarardila@hotmail.com>
2024-02-10 13:12:13 -0500

    Complete: i18nPlural Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -2,20 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pages-uncommon-page',
-  templateUrl: './uncommon-page.component.html'
+  templateUrl: './uncommon-page.component.html',
 })
 export class UncommonPageComponent {
-public name: string = 'Fabio'
-public gender: 'male' | 'female' = 'male'
-public genderMap = {
-  male : 'invitarlo',
-  female: 'invitarla'
-}
+  //i18n Select
+  public name: string = 'Fabio';
+  public gender: 'male' | 'female' = 'male';
+  public genderMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
-changeClient(){
-  this.name = "Yulieth"
-  this.gender = "female"
+  changeClient() {
+    this.name = 'Yulieth';
+    this.gender = 'female';
+  }
 
-}
+  //i18n Plural
+  public clientes: string[] = [
+    'Fabio',
+    'Yulieth',
+    'Alejandro',
+    'FabioF',
+    'Laura',
+    'Giovanni',
+  ];
 
+  public clientesMap = {
+    '=0' : 'no tenemos clientes',
+    '=1' : 'tenemos 1 cliente',
+    'other' : 'tenemos # clientes'
+  }
+
+  delClient() {
+    this.clientes.shift();
+  }
 }
