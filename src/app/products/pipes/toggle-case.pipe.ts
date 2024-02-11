a61299d35c82e8ba31b3fc49fb48bca74b03c7ce
Fabio <fabioescobarardila@hotmail.com>
2024-02-11 09:20:37 -0500

    Complete: Pipe personalizado - argumentos definidos

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -4,15 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'toggleCase',
 })
 export class ToggleCase implements PipeTransform {
-  transform(value: string) {
-    let strBuilder: string = '';
-    for (let letter of value) {
-      if (letter.match('[A-Z]')) {
-        strBuilder += letter.toLowerCase();
-      } else {
-        strBuilder += letter.toUpperCase();
-      }
-    }
-    return strBuilder;
+  transform(value: string, upper: boolean = false, ...args: any[]) {
+    console.log(args);
+    return upper ? value.toUpperCase() : value.toLowerCase();
   }
+
+  // transform(value: string) {
+  //   let strBuilder: string = '';
+  //   for (let letter of value) {
+  //     if (letter.match('[A-Z]')) {
+  //       strBuilder += letter.toLowerCase();
+  //     } else {
+  //       strBuilder += letter.toUpperCase();
+  //     }
+  //   }
+  //   return strBuilder;
+  // }
 }
